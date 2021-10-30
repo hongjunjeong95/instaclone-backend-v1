1821f0187f0f8c4027d3b04802fd3d9a3ed383ba
hongjunjeong95 <hongjunjeong95@gmail.com>
2021-10-30 10:38:06 +0900

    Delet movies and prisma

--- schema.js
+++ schema.js
@@ -5,9 +5,6 @@ import {
   makeExecutableSchema,
 } from "graphql-tools";
 import path from "path";
-// import { loadFilesSync } from "@graphql-tools/load-files";
-// import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
-// import { makeExecutableSchema } from "apollo-server-express";
 
 const loadedTypes = loadFilesSync(path.join(__dirname, "**", "*.typeDefs.js"));
 const loadedResolvers = loadFilesSync(
