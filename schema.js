96696d467e08be02cb430aa13abec360d744699c
hongjunjeong95 <hongjunjeong95@gmail.com>
2021-11-01 13:03:25 +0900

    Add bio and avatar and file upload setting one

--- schema.js
+++ schema.js
@@ -1,20 +1,9 @@
-import {
-  loadFilesSync,
-  mergeTypeDefs,
-  mergeResolvers,
-  makeExecutableSchema,
-} from "graphql-tools";
+import { loadFilesSync, mergeTypeDefs, mergeResolvers } from "graphql-tools";
 import path from "path";
 
 const loadedTypes = loadFilesSync(path.join(__dirname, "**", "*.typeDefs.js"));
 const loadedResolvers = loadFilesSync(
   path.join(__dirname, "**", "*.resolvers.js")
 );
-
-const typeDefs = mergeTypeDefs(loadedTypes);
-const resolvers = mergeResolvers(loadedResolvers);
-
-// @ts-ignore
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-export default schema;
+export const typeDefs = mergeTypeDefs(loadedTypes);
+export const resolvers = mergeResolvers(loadedResolvers);
