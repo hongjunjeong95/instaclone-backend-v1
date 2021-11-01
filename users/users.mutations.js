28e833a96fd3b42102bed55c1f7534e9e37dfd2b
hongjunjeong95 <hongjunjeong95@gmail.com>
2021-11-01 10:22:30 +0900

    Create seeProfile and login api

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../client";
 
 export default {
@@ -21,7 +22,7 @@ export default {
           },
         });
         if (existingUser) {
-          throw new Error("This username/password is already taken.");
+          throw new Error("This username/email is already taken.");
         }
         const uglyPassword = await bcrypt.hash(password, 10);
         return client.user.create({
@@ -33,10 +34,30 @@ export default {
             password: uglyPassword,
           },
         });
-        return user;
       } catch (e) {
         return e;
       }
+    },
+    login: async (_, { username, password }) => {
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password.",
+        };
+      }
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
