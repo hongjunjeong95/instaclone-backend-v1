4b22f6babcb676df56e8ee54581dabdfa64a8ca6
hongjunjeong95 <hongjunjeong95@gmail.com>
2021-12-30 18:23:37 +0900

    Ejecting from apollo server

--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,10 @@
-import { IResolvers } from "apollo-server-express";
-import { loadFilesSync, mergeTypeDefs, mergeResolvers } from "graphql-tools";
-import path from "path";
+import { IResolvers } from 'apollo-server-express';
+import { loadFilesSync, mergeTypeDefs, mergeResolvers } from 'graphql-tools';
+import path from 'path';
 
-const loadedTypes = loadFilesSync(path.join(__dirname, "**", "*.typeDefs.ts"));
+const loadedTypes = loadFilesSync(path.join(__dirname, '**', '*.typeDefs.ts'));
 const loadedResolvers = loadFilesSync(
-  path.join(__dirname, "**", "*.resolvers.ts")
+  path.join(__dirname, '**', '*.resolvers.ts')
 );
 export const typeDefs = mergeTypeDefs(loadedTypes);
 export const resolvers = mergeResolvers(loadedResolvers) as
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-require("dotenv").config();
-import { ApolloServer } from "apollo-server";
+require('dotenv').config();
+import express from 'express';
+import logger from 'morgan';
+import { ApolloServer } from 'apollo-server-express';
 
-import client from "./client";
-import { typeDefs, resolvers } from "./schema";
-import { getUser } from "./users/users.utils";
+import client from './client';
+import { typeDefs, resolvers } from './schema';
+import { getUser } from './users/users.utils';
+import { IResolvers } from 'graphql-tools';
 
 const PORT = process.env.PORT;
 
@@ -18,8 +21,16 @@ const server = new ApolloServer({
   },
 });
 
-server
-  .listen(PORT)
-  .then(() =>
-    console.log(`🚀Server is running on http://localhost:${PORT} ✅`)
-  );
+// server
+//   .listen(PORT)
+//   .then(() =>
+//     console.log(`🚀Server is running on http://localhost:${PORT} ✅`)
+//   );
+// );
+
+const app = express();
+app.use(logger('tiny'));
+server.applyMiddleware({ app });
+app.listen({ port: PORT }, () => {
+  console.log(`🚀Server is running on http://localhost:${PORT} ✅`);
+});
